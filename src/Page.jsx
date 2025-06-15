d06ccc507ab1abfef4d70cee037f6d3abede2f22
Barack <summonbarack@gmail.com>
2025-06-16 00:23:27 +0100

    Searching done

---
+++ src/Page.jsx
@@ -0,0 +1,221 @@
+import { useState, useEffect, use } from "react";
+import Axios from "axios";
+import formatDistance from "date-fns/formatDistanceToNow";
+
+import desktopImg from "./assets/hero-image-github-profile.jpg";
+import phoneImg from "./assets/hero-image-github-profile-sm.jpg";
+import searchIcon from "./assets/Search.svg";
+import forkIcon from "./assets/Nesting.svg";
+import starIcon from "./assets/Star.svg";
+import chieldIcon from "./assets/Chield_alt.svg";
+
+const Page = () => {
+  const [username, setUsername] = useState("");
+  const [selected, setSelected] = useState("");
+  const [fetch, setFetch] = useState(false);
+
+  const [repos, setRepos] = useState([]);
+  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [searchSuggests, setSearchSuggests] = useState([]);
+
+  const URL = `https://api.github.com/users`;
+  const URL1 = `https://api.github.com/users/${selected}`;
+  const [reposURL, setReposURL] = useState("");
+
+  const fetchUsers = async () => {
+    try {
+      const res = await Axios.get(URL);
+      setUsers(res.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  const fetchUser = async () => {
+    try {
+      const res = await Axios.get(URL1);
+      setUser(res.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  const fetchrepos = async () => {
+    try {
+      const res = await Axios.get(reposURL);
+      setRepos(res.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (username.length > 0) {
+      fetchUsers();
+      setSearchSuggests(users.filter((user) => user.login.includes(username)));
+    }
+    if (selected !== "") {
+      fetchUser();
+      fetchrepos();
+    }
+    console.log(user);
+  }, [username, selected]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setFetch(true);
+    setSelected(username);
+    setReposURL(`https://api.github.com/users/${username}/repos`);
+  };
+
+  return (
+    <div className="relative w-screen text-[#CDD5E0]">
+      <img
+        src={desktopImg}
+        alt="Cover-image"
+        className="w-full h-[15rem] object-cover"
+      />
+      <div className="absolute top-0 left-[50%] transform -translate-x-[50%] w-[80%] h-full flex flex-col">
+        <div className="bg-[#364153] absolute z-20 w-[90%] sm:w-[60%] md:w-[80%] h-auto top-12 left-[50%] transform -translate-x-[50%] rounded-2xl py-2">
+          <form
+            onSubmit={handleSearch}
+            className="w-full h-16 flex justify-center items-center"
+          >
+            <button type="submit">
+              <img
+                src={searchIcon}
+                alt="Search"
+                className="mx-3 w-auto h-auto"
+              />
+            </button>
+            <input
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              placeholder="Enter username"
+              className="w-[90%] h-full bg-transparent focus:outline-none"
+            />
+          </form>
+
+          {/* Search suggestion */}
+          <div
+            className={
+              username.length > 1
+                ? "bg-[#364153] w-auto h-auto flex justify-center items-center flex-col mx-4 pt-4"
+                : "hidden"
+            }
+          >
+            {searchSuggests.map((suggests) => (
+              <li
+                key={suggests.id}
+                onClick={() => {
+                  setSelected(suggests.login);
+                  setReposURL(suggests.repos_url);
+                  setUsername("");
+                }}
+                className="w-full h-auto border-t border-[#97a3b6] flex justify-start items-center gap-2 py-2 px-2 cursor-pointer hover:scale-95 transition duration-300"
+              >
+                <img
+                  src={suggests.avatar_url}
+                  alt="image"
+                  className="w-12 h-w-12 rounded-full"
+                />
+                <div className="border-l font-semibold px-2">
+                  {suggests.login}
+                </div>
+              </li>
+            ))}
+          </div>
+        </div>
+
+        {/* User profile */}
+        <div
+          className={
+            fetch ? "w-full h-40 mt-44 flex items-end gap-10" : "hidden"
+          }
+        >
+          <img
+            src={user.avatar_url}
+            alt="Profile"
+            width={100}
+            height={100}
+            className="w-48 h-full border-[#364153] border-8 rounded-2xl mr-10"
+          />
+          <div className="w-auto h-[30%] flex justify-around gap-3 items-center px-3 bg-[#111729] rounded-xl text-lg">
+            <strong>Followers</strong>
+            <div className="border-l border-l-[#97A3B6] h-[70%]"></div>
+            <strong>{user.followers}</strong>
+          </div>
+          <div className="w-auto h-[30%] flex justify-around gap-3 items-center px-3 bg-[#111729] rounded-xl text-lg">
+            <strong>Following</strong>
+            <div className="border-l border-l-[#97A3B6] h-[70%]"></div>
+            <strong>{user.following}</strong>
+          </div>
+          <div
+            className={
+              user.location == null
+                ? "hidden"
+                : "w-auto h-[30%] flex justify-around gap-3 items-center px-3 bg-[#111729] rounded-xl text-lg"
+            }
+          >
+            <strong>Location</strong>
+            <div className="border-l border-l-[#97A3B6] h-[70%]"></div>
+            <strong>{user.location}</strong>
+          </div>
+        </div>
+        <div className={fetch ? "pt-4 w-[50%] h-auto mb-20" : "hidden"}>
+          <h1 className="text-5xl font-extrabold mb-2">{user.name}</h1>
+        </div>
+
+        {/* User repo */}
+        <div
+          className={
+            fetch
+              ? "w-full h-auto grid lg:grid-cols-2 gap-8 mb-10 cursor-pointer"
+              : "hidden"
+          }
+        >
+          {Array.isArray(repos) &&
+            repos.map((repo) => (
+              <li
+                key={repo.id}
+                onClick={() => {
+                  window.location.href = repo.html_url;
+                }}
+                className="bg-[#1D1B48] h-auto flex flex-col p-4 rounded-3xl md:rounded-xl"
+              >
+                <div className="flex flex-col gap-3">
+                  <strong className="font-extrabold text-2xl">
+                    {repo.name}
+                  </strong>
+                  <div>
+                    <strong>Visibility: </strong>
+                    {repo.visibility}
+                  </div>
+                  <div className="w-full h-auto flex justify-start gap-3">
+                    <div className="w-auto h-full flex items-center gap-1">
+                      <img src={forkIcon} alt="" />
+                      {repo.forks_count}
+                    </div>
+                    <div className="w-auto h-full flex items-center gap-1">
+                      <img src={starIcon} alt="" />
+                      {repo.stargazers_count}
+                    </div>
+                    <div className="w-auto h-full flex items-center gap-1">
+                      {formatDistance(new Date(repo.created_at), new Date(), {
+                        addSuffix: true,
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
